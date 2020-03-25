e9fb5164e9a06198c68052797adc4942f39fa8b3
Archie L. Cobbs <archie.cobbs@gmail.com>
2020-03-25 17:50:26 -0500

    New bug test case.

--- orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -3,7 +3,13 @@ package org.hibernate.bugs;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.Root;
 
+import org.hibernate.bugs.Foo_;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -31,7 +37,18 @@ public class JPAUnitTestCase {
 	public void hhh123Test() throws Exception {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-		// Do stuff...
+
+        final CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        final CriteriaQuery<Foo> cq = cb.createQuery(Foo.class);
+        final Root<Foo> foo = cq.from(Foo.class);
+        cq.select(foo)
+          .where(cb.lessThanOrEqualTo(
+              cb.function("TIME", String.class, foo.get(Foo_.startTime)), "17:00:00"));
+
+        final TypedQuery<Foo> tq = entityManager.createQuery(cq);
+
+        tq.getResultStream();
+
 		entityManager.getTransaction().commit();
 		entityManager.close();
 	}
